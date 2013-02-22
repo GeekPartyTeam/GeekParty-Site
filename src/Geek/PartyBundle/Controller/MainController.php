68ae0903e048bb894445bfb1c12ed4819520847f
kipelovets <kipelovets@mail.ru>
2013-02-23 03:33:10 +0400

    WIP

---
+++ src/Geek/PartyBundle/Controller/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Geek\PartyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('GeekPartyBundle:Main:index.html.twig');
+    }
+
+    public function peopleAction()
+    {
+        return $this->render('GeekPartyBundle:Main:people.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('GeekPartyBundle:Main:about.html.twig');
+    }
+
+    public function worksAction()
+    {
+        return $this->render('GeekPartyBundle:Main:works.html.twig');
+    }
+}
--- web/js/countdown.js
+++ web/js/countdown.js
@@ -1,8 +1,9 @@
 $(function() {
     var targetData = new Date(2013,1,23,12,1);
+    var timer;
 
     var setTimer =  function(){
-        var remaining = new Date - targetData,
+        var remaining = targetData - new Date,
             milli = 864e5,
             a_days = remaining / milli,
             days = Math.floor(a_days),
@@ -12,9 +13,15 @@ $(function() {
             a_minutes = (a_hours - hours) * 60,
             seconds = Math.floor((a_minutes - minutes) * 60);
 
+        if (remaining < 0) {
+            $('#timer').hide();
+            clearInterval(timer);
+            return;
+        }
+
         $('#days').html((days < 10) ? '0' + days : days);
         $('#hours').html((hours < 10) ? '0' + hours : hours);
-        $('#minutes')html((minutes < 10) ? '0' + minutes : minutes);
+        $('#minutes').html((minutes < 10) ? '0' + minutes : minutes);
     };
 
     setTimer();
