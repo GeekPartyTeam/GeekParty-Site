cf3d2348bcb31c76297004cb9524acce0bfefc29
kipelovets <kipelovets@mail.ru>
2013-04-14 02:35:53 +0400

    default timezone

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+date_default_timezone_set('Europe/Moscow');
+
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
