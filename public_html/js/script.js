9de3794e224a9f14015c927bce2292ad01a35ed0
Коршунов Георгий <georgy.k@nevosoft.ru>
2013-06-03 13:12:38 +0400

    merge

---
+++ public_html/js/script.js
@@ -0,0 +1,36 @@
+!function () {
+    var fbAuthStatus = null
+        , urlPrefix = document.location.href.replace(/http(s)?:\/\/[^\/]+(\/app(_dev)?.php)?.*/, '$2')
+        ;
+
+    function goLogIn(){
+        window.location.href = urlPrefix + "/login_check";
+    }
+
+    window.onFbInit = function () {
+        FB.getLoginStatus(function (r) {
+            FB.Event.subscribe('auth.statusChange', function(response) {
+                if (response.session || response.authResponse) {
+                    setTimeout(goLogIn, 500);
+                } else {
+                    window.location.href = urlPrefix + "/logout";
+                }
+            });
+
+            fbAuthStatus = r;
+
+            $(function () {
+                $('.facebook-login').show().click(fbSignIn);
+                $('.facebook-wait').hide();
+            })
+        })
+    }
+
+    function fbSignIn() {
+        if (fbAuthStatus.status != 'connected') {
+            FB.login()
+        } else {
+            goLogIn()
+        }
+    }
+}()
