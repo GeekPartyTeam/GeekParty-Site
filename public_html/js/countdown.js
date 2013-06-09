3a4423b2ad49bde89e2c737ad8ec09884abca0df
kipelovets <kipelovets@mail.ru>
2013-06-09 20:12:47 +0400

    counter fix for ios

--- public_html/js/countdown.js
+++ public_html/js/countdown.js
@@ -1,33 +1,37 @@
 $(function() {
-    var targetDate = window.targetDate ? new Date(window.targetDate) : null;
-    var timer;
+    if (window.targetDate) {
+        var arr = window.targetDate.split(/[- :]/)
+            , targetDate = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5])
+            , timer
+            ;
 
-    if (targetDate - new Date >= 0) {
-        var setTimer =  function(){
-            var remaining = targetDate - new Date,
-                milli = 864e5,
-                a_days = remaining / milli,
-                days = Math.floor(a_days),
-                a_hours = (a_days - days) * 24,
-                hours = Math.floor(a_hours),
-                minutes = Math.floor((a_hours - hours) * 60),
-                a_minutes = (a_hours - hours) * 60,
-                seconds = Math.floor((a_minutes - minutes) * 60);
+        if (targetDate - new Date >= 0) {
+            var setTimer =  function(){
+                var remaining = targetDate - new Date,
+                    milli = 864e5,
+                    a_days = remaining / milli,
+                    days = Math.floor(a_days),
+                    a_hours = (a_days - days) * 24,
+                    hours = Math.floor(a_hours),
+                    minutes = Math.floor((a_hours - hours) * 60),
+                    a_minutes = (a_hours - hours) * 60,
+                    seconds = Math.floor((a_minutes - minutes) * 60);
 
-            if (remaining < 0) {
-                $('#timer').hide();
-                clearInterval(timer);
-                window.location.reload();
-                return;
-            }
+                if (remaining < 0) {
+                    $('#timer').hide();
+                    clearInterval(timer);
+                    window.location.reload();
+                    return;
+                }
 
-            $('#days').html((days < 10) ? '0' + days : days);
-            $('#hours').html((hours < 10) ? '0' + hours : hours);
-            $('#minutes').html((minutes < 10) ? '0' + minutes : minutes);
-        };
+                $('#days').html((days < 10) ? '0' + days : days);
+                $('#hours').html((hours < 10) ? '0' + hours : hours);
+                $('#minutes').html((minutes < 10) ? '0' + minutes : minutes);
+            };
 
-        setTimer();
+            setTimer();
 
-        timer = window.setInterval(setTimer, 1000);
+            timer = window.setInterval(setTimer, 1000);
+        }
     }
 })
