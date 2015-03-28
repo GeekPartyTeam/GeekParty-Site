66cf23f1dbf64cfff995b39974a93e44258e51ee
kipelovets <kipelovets@mail.ru>
2015-03-29 02:50:34 +0300

    Party list

--- src/Geek/PartyBundle/Controller/PartyController.php
+++ src/Geek/PartyBundle/Controller/PartyController.php
@@ -3,12 +3,8 @@
 namespace Geek\PartyBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Geek\PartyBundle\Entity\Party;
-use Geek\PartyBundle\Form\PartyType;
 
 /**
  * Party controller.
@@ -24,4 +20,25 @@ class PartyController extends Base\CRUDController
     {
         return 'admin_parties';
     }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return array
+     *
+     * @Template()
+     */
+    public function indexAction(/** @noinspection PhpUnusedParameterInspection */
+        Request $request, $id)
+    {
+        $entities = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('GeekPartyBundle:Party')
+            ->findBy([], ['startTime' => 'DESC']);
+
+        return $this->renderPage('index', [
+            'entities' => $entities,
+        ]);
+    }
+
 }
