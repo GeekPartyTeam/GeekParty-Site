9963822ab7b4ac17f59dc49fc6c6e9055e04b46d
kipelovets <kipelovets@mail.ru>
2015-07-26 22:34:56 +0300

    Using more bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
             new Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle(),
             new Prism\PollBundle\PrismPollBundle(),
+            new Fkr\CssURLRewriteBundle\FkrCssURLRewriteBundle(),
             //
             new Geek\PartyBundle\GeekPartyBundle(),
         );
